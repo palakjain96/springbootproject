b5f431096d6d4a7ec1db26f9920561f53d336171
palajain <palajain@adobe.com>
2022-10-14 22:48:37 +0530

    initial code

---
+++ src/main/java/com/example/springbootproject/dao/PersonDao.java
@@ -0,0 +1,13 @@
+package com.example.springbootproject.dao;
+
+import com.example.springbootproject.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+    default int insertPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id,person);
+    }
+}
---
+++ src/main/java/com/example/springbootproject/service/PersonService.java
@@ -0,0 +1,21 @@
+package com.example.springbootproject.service;
+
+import com.example.springbootproject.dao.PersonDao;
+import com.example.springbootproject.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+    }
+}
